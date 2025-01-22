917c05c45c38fef849ad455d9f66e6ea47fb39c1
Vinay Sanga <vinay.sanga@aholadigital.com>
2025-01-22 13:27:03 +0100

    Make the users table scrollable

--- src/components/users-table.svelte
+++ src/components/users-table.svelte
@@ -8,52 +8,60 @@
   } = $props();
 </script>
 
-<div class="border-base-content/25 rounded-box w-full overflow-x-auto border">
-  <table class="table">
-    <thead>
-      <tr>
-        {#each tableHeaders as header}
-          <th>{header}</th>
-        {/each}
-      </tr>
-    </thead>
-    <tbody>
-      {#each tableData as row}
+<div class="border-base-content/10 rounded-box border p-2">
+  <div class="enhanced-scrollbar max-h-[26rem] overflow-x-auto overflow-y-auto [&::-webkit-scrollbar]:h-2">
+    <table class="table my-0">
+      <thead class="bg-base-100 sticky top-0 z-30">
         <tr>
-          <td class="text-nowrap">{row.username}</td>
-          <td>
-            <label class="form-control flex items-center gap-1">
-              <input type="checkbox" class="switch switch-error" checked={row.restricted} 
-              onchange={async (e: Event) => {
-                e.preventDefault();
-                const restricted = (e.target as HTMLInputElement).checked;
-                const res = await fetch("/api/user/update-restricted", {
-                  method: "PATCH",
-                  body: JSON.stringify({ username: row.username, restricted }),
-                });
-                await res.json();
-                window.location.reload();
-              }}
-              />
-            </label>
-          </td>
-          <td class="text-nowrap">
-            <button
-              class="btn btn-circle btn-text btn-sm"
-              aria-label="Action button"
-              onclick={async (e) => {
-                e.preventDefault();
-                const res = await fetch("/api/user/delete", {
-                  method: "DELETE",
-                  body: JSON.stringify({ username: row.username }),
-                });
-                await res.json();
-                window.location.reload();
-              }}><span class="icon-[tabler--trash]"></span></button
-            >
-          </td>
+          {#each tableHeaders as header}
+            <th>{header}</th>
+          {/each}
         </tr>
-      {/each}
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        {#each tableData as row}
+          <tr class="border-base-content/10">
+            <td class="px-4 whitespace-nowrap">{row.username}</td>
+            <td class="px-4">
+              <label class="form-control flex items-center gap-1">
+                <input
+                  type="checkbox"
+                  class="switch switch-error"
+                  checked={row.restricted}
+                  onchange={async (e: Event) => {
+                    e.preventDefault();
+                    const restricted = (e.target as HTMLInputElement).checked;
+                    const res = await fetch("/api/user/update-restricted", {
+                      method: "PATCH",
+                      body: JSON.stringify({
+                        username: row.username,
+                        restricted,
+                      }),
+                    });
+                    await res.json();
+                    window.location.reload();
+                  }}
+                />
+              </label>
+            </td>
+            <td class="px-4 whitespace-nowrap">
+              <button
+                class="btn btn-circle btn-text btn-sm"
+                aria-label="Action button"
+                onclick={async (e) => {
+                  e.preventDefault();
+                  const res = await fetch("/api/user/delete", {
+                    method: "DELETE",
+                    body: JSON.stringify({ username: row.username }),
+                  });
+                  await res.json();
+                  window.location.reload();
+                }}><span class="icon-[tabler--trash]"></span></button
+              >
+            </td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
 </div>
